1a3c1a48fb7abaca80edd5f21f84fc00eb96fc93
ozlaw <omclawrence@gmail.com>
2024-10-17 09:25:33 -0400

    Remove html link

--- tag-cakephp4.4/src/templates/Storagelocations/view.php
+++ tag-cakephp4.4/src/templates/Storagelocations/view.php
@@ -21,7 +21,7 @@
             <table>
                 <tr>
                     <th><?= __('User') ?></th>
-                    <td><?= $storagelocation->has('user') ? $this->Html->link($storagelocation->user->first_name . " " . $storagelocation->user->last_name, ['controller' => 'user', 'action' => 'view', $storagelocation->user->id]) : '' ?></td>
+                    <td><?= h($storagelocation->user->first_name . " " . $storagelocation->user->last_name)?></td>
                 </tr>
                 <tr>
                     <th><?= __('Name') ?></th>
